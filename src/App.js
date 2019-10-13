5b302510a7bc32f8f486ebd24c6d6c8942d5dc26
Shahriar Elias <shahriar998@gmail.com>
2019-10-12 23:07:38 -0400

    final push

--- src/App.js
+++ src/App.js
@@ -1,39 +1,75 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import React, { Component } from 'react';
+import './App.css';
+import PictureCard from "./components/PictureCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import friends from "./cards.json";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  
   state = {
-    friends
+    friends,
+    clickedArray: [],
+    topScore: 0,
+    score: 0,
+    message: "",
+    
   };
-
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  clickPicture = id => {
+   
+    const manupulateArray = this.manupulateArray(friends);
+    this.setState({friends: manupulateArray});
+    
+    if (this.state.clickedArray.includes(id)) {
+      this.setState({ score: 0, clickedArray: [], message: "Incorrect!! You Failed!"});
+    }
+    else {
+      this.setState({
+        clickedArray: this.state.clickedArray.concat([id]),
+        score: this.state.score + 1,
+        message: "Correct!!",
+        shakeit: "false"
+      });
+    }
+   
+    if (this.state.score > this.state.topScore) {
+      this.setState({ topScore: this.state.score });
+    }
+   
+  }
+  manupulateArray = (picturesArray) => {
+      for (let i = picturesArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [picturesArray[i], picturesArray[j]] = [picturesArray[j], picturesArray[i]];
+      }
+      return picturesArray;
+  }
   render() {
     return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
+      <div className="App">
+        <div className="card-header bg-primary text-white">
+          Click an Image to begin! You lose if you click on an image twice
+          <p className = "score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
+          <p className="message"><strong>{this.state.message}</strong></p>
+        </div>
+        <Wrapper
+        
+        pictures=
+          {this.state.friends.map(picture => (
+            <PictureCard
+              clickPicture={this.clickPicture}
+              id={picture.id}
+              key={picture.id} // to get rid of unique key prop warning
+              name={picture.name}
+              image={picture.image}
+            />
+          ))}
+        />
+        <footer className="footer">
+      <div className="container">
+        <span className="text-muted">&copy;SBE Alright Reserved</span>
+      </div>
+    </footer> 
+      </div>
     );
   }
 }
